d6b4f383a17ca6d75373173b47e46d3632f39aa8
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-08-16 21:36:53 +0900

    auto commit

--- src/components/ContextSample.tsx
+++ src/components/ContextSample.tsx
@@ -11,3 +11,23 @@ const Title = () => {
         </TitleContext.Consumer>
     )
 }
+
+const Header = () => {
+    return (
+        <div>
+            <Title />
+        </div>
+    )
+}
+
+const Page = () => {
+    const title = 'React Book'
+
+    return (
+        <TitleContext.Provider value="{title}">
+            <Header />
+        </TitleContext.Provider>
+    )
+}
+
+export default Page
