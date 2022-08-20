d55ffb169d9af2511fba8b9d655cba8033c7a634
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-08-20 14:42:18 +0900

    auto commit

--- src/components/ContextSample.tsx
+++ src/components/ContextSample.tsx
@@ -1,20 +1,38 @@
-import React from "react"
+import React, { useReducer } from "react"
 import { useState } from 'react';
+
+type  Action = `DECREMENT` | `INCREMENT` | `DOUBLE` | `RESET`
+
+const reducer = (currentCount: number, action: Action) => {
+    switch (action) {
+        case `INCREMENT`:
+            return currentCount + 1
+        case `DECREMENT`:
+            return currentCount -1
+        case `DOUBLE`:
+            return currentCount * 2
+        case `RESET`:
+            return 0
+        default:
+            return currentCount
+    }
+}
 
 type CounterProps = {
     initialValue: number
 }
 
 const Counter = (props: CounterProps) => {
-    const {initialValue} = props
-
-    const [count, setCount] = useState(initialValue)
+    const {initialValue } = props
+    const [count, dispatch] = useReducer(reducer, initialValue)
 
     return (
         <div>
             <p>Count: {count}</p>
-            <button onClick={() => setCount(count -1)}>-</button>
-            <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+            <button onClick={() => dispatch('DECREMENT')}>-</button>
+            <button onClick={() => dispatch('INCREMENT')}>+</button>
+            <button onClick={() => dispatch('DOUBLE')}>x2</button>
+            <button onClick={() => dispatch('RESET')}>Reset</button>
         </div>
     )
 }
